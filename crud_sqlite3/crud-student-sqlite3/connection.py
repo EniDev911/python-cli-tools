cdbe3381b39b62ad1bc61a34adaa466e14f60360
EniDev911 <enidev911@gmail.com>
2022-01-16 15:09:03 -0300

    up

---
+++ crud_sqlite3/crud-student-sqlite3/connection.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+DB = "university.db"
+
+class Database:
+
+	def run_query(self, query, parameters=()):
+		
--- test/color_terminal.py
+++ test/color_terminal.py
@@ -1,9 +1,25 @@
-class bcolors:
-    OK = '\033[92m' #GREEN
-    WARNING = '\033[93m' #YELLOW
-    FAIL = '\033[91m' #RED
-    RESET = '\033[0m' #RESET COLOR
+#class bcolors:
+#    OK = '\033[92m' #GREEN
+#    WARNING = '\033[93m' #YELLOW
+#    FAIL = '\033[91m' #RED
+#    RESET = '\033[0m' #RESET COLOR
 
-print(bcolors.OK + "File Saved Successfully!" + bcolors.RESET)
-print(bcolors.WARNING + "Warning: Are you sure you want to continue?" + bcolors.RESET)
-print(bcolors.FAIL + "Unable to delete record." + bcolors.RESET)
+#print(bcolors.OK + "File Saved Successfully!" + bcolors.RESET)
+#print(bcolors.WARNING + "Warning: Are you sure you want to continue?" + bcolors.RESET)
+#print(bcolors.FAIL + "Unable to delete record." + bcolors.RESET)
+
+import sys
+from termcolor import colored, cprint
+
+text = colored('Hello, World!', 'red', attrs=['reverse', 'blink'])
+print(text)
+cprint('Hello, World!', 'green', 'on_red')
+
+print_red_on_cyan = lambda x: cprint(x, 'red', 'on_cyan')
+print_red_on_cyan('Hello, World!')
+print_red_on_cyan('Hello, Universe!')
+
+for i in range(10):
+    cprint(i, 'magenta', end=' ')
+
+cprint("Attention!", 'red', attrs=['bold'], file=sys.stderr)
